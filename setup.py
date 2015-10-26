14053dfa51068b9f2e37dac1189d5bfa4f07eb6d
Douglas Bastos <douglashsb@gmail.com>
2015-10-26 16:22:34 -0200

    Cria arquivo setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+from setuptools import find_packages, setup
+
+setup(
+    name='votacao_with_redis',
+    description='None',
+    author='Douglas Bastos',
+    author_email='douglashsb@gmail.com',
+    url='https://github.com/douglasbastos/votacao_with_redis',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[]
+)
