db26b1b6ea427be39d88f12e1423de540619c88c
Douglas Bastos <douglashsb@gmail.com>
2015-10-26 18:34:44 -0200

    View inclui dados no redis

--- votacao_with_redis/views.py
+++ votacao_with_redis/views.py
@@ -3,18 +3,28 @@ import random
 from django.shortcuts import render
 from .models import Poll, Option
 
+import redis
+cache = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
 
 def incluir_voto_mysql(request):
     options = [1, 2, 3, 4]
     result = random.choice(options)
     option = Option.objects.filter(id=result)[0]
-
+    print result
+    print '============'
     option.votes += 1
     option.save()
-    context = {'text': 'Seu voto foi realizado com sucesso'}
+    context = {'text': 'Seu voto foi realizado com sucesso no MYSQL'}
 
     return render(request, 'votacao_with_redis/voto_ok.html', context)
 
 
-def incluir_voto_redis(request, **kwargs):
-    pass
+def incluir_voto_redis(request):
+    options = [1, 2, 3, 4]
+    result = random.choice(options)
+    print result
+    print '============'
+    cache.incr('votacao:option:{}'.format(result))
+    context = {'text': 'Seu voto foi realizado com sucesso no REDIS'}
+
+    return render(request, 'votacao_with_redis/voto_ok.html', context)
