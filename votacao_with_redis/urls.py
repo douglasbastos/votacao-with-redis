7fd7ebee920c879234baf61fce7f594ba5303f46
Douglas Bastos <douglashsb@gmail.com>
2015-10-26 18:15:16 -0200

    Url cadastra voto

--- votacao_with_redis/urls.py
+++ votacao_with_redis/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<id>[0-9]+)/vote$', views.DetailPost.as_view(), name='vote'),
-    url(r'^(?P<id>[0-9]+)/resultado$', views.DetailPost.as_view(), name='vote'),
+    url(r'^vote/$', views.incluir_voto_mysql, name='vote'),
+    # url(r'^(?P<id>[0-9]+)/resultado$', views.DetailPost.as_view(), name='vote'),
 ]
