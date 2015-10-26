786614e33fd3c69b0f32e43f40b23351471925ca
Douglas Bastos <douglashsb@gmail.com>
2015-10-26 19:16:40 -0200

    Urls para mostrar resultado da consulta

--- votacao_with_redis/urls.py
+++ votacao_with_redis/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^mysql/vote/$', views.incluir_voto_mysql, name='vote_mysql'),
     url(r'^redis/vote/$', views.incluir_voto_redis, name='vote_redis'),
+    url(r'^mysql/resultado/$', views.resultado_mysql, name='resultado_mysql'),
+    url(r'^redis/resultado/$', views.resultado_redis, name='resultado_redis'),
+
 ]
