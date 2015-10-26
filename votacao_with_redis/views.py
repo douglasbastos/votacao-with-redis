80aba38402d739c47e1eb632862759a923b1c27a
Douglas Bastos <douglashsb@gmail.com>
2015-10-26 18:14:44 -0200

    View para inserção de voto

--- votacao_with_redis/views.py
+++ votacao_with_redis/views.py
@@ -1,3 +1,20 @@
+# coding: utf-8
+import random
 from django.shortcuts import render
+from .models import Poll, Option
 
-# Create your views here.
+
+def incluir_voto_mysql(request):
+    options = [1, 2, 3, 4]
+    result = random.choice(options)
+    option = Option.objects.filter(id=result)[0]
+
+    option.votes += 1
+    option.save()
+    context = {'text': 'Seu voto foi realizado com sucesso'}
+
+    return render(request, 'votacao_with_redis/voto_ok.html', context)
+
+
+def incluir_voto_redis(request, **kwargs):
+    pass
