cc58a10411ff4ccfc3002e4d9a6735130a209419
Douglas Bastos <douglashsb@gmail.com>
2015-10-26 18:56:59 -0200

    Adiciona never_cache nas views

--- votacao_with_redis/views.py
+++ votacao_with_redis/views.py
@@ -1,30 +1,32 @@
 # coding: utf-8
 import random
 from django.shortcuts import render
+from django.views.decorators.cache import never_cache
 from .models import Poll, Option
 
 import redis
 cache = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
 
+
+@never_cache
 def incluir_voto_mysql(request):
     options = [1, 2, 3, 4]
     result = random.choice(options)
     option = Option.objects.filter(id=result)[0]
-    print result
-    print '============'
     option.votes += 1
     option.save()
-    context = {'text': 'Seu voto foi realizado com sucesso no MYSQL'}
+
+    context = {'text': 'Seu voto foi realizado com sucesso no MySQL'}
 
     return render(request, 'votacao_with_redis/voto_ok.html', context)
 
 
+@never_cache
 def incluir_voto_redis(request):
     options = [1, 2, 3, 4]
     result = random.choice(options)
-    print result
-    print '============'
     cache.incr('votacao:option:{}'.format(result))
+
     context = {'text': 'Seu voto foi realizado com sucesso no REDIS'}
 
     return render(request, 'votacao_with_redis/voto_ok.html', context)
